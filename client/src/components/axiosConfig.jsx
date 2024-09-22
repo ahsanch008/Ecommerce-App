b737936608efca390529a0c63cc667f3f6fe2da6
ahsanch008 <ahsan788766@gmail.com>
2024-09-22 19:42:40 +0500

    ..

--- client/src/components/axiosConfig.jsx
+++ client/src/components/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const isDevelopment = window.location.hostname === 'localhost';
 const baseURL = isDevelopment 
   ? 'http://localhost:3000'
-  : 'https://demo-ecommerce-server-bay.vercel.app'; // Replace with your local IP address if needed
+  : 'https://demo-ecomm-server.vercel.app/'; // Replace with your local IP address if needed
 
 const instance = axios.create({
   baseURL: baseURL,
